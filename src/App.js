769df878ca93cb9a3676e09f7ade26c18122c66c
Gustavo Reis <GustavoReisG17@outlook.com>
2024-04-12 18:06:18 -0300

    Header e ImageBackground adicionados - Primeiros Passos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/js/header';
+import BodyComponent from './components/js/bodyComponent';
 
 function App() {
   return (
     <div className="MainDiv">
       <Header />
+      <BodyComponent />
     </div>
   );
 }
--- src/components/js/header.js
+++ src/components/js/header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import logoImg from '../imgs/GmPlanetLogo.png';
+import '../css/header.css';
 
-// Definição do componente Header
 function Header() {
   return (
-    <header>
-      <img></img>
+    <header id="header">
+      <img class="Headers" src={logoImg} alt="Logo" id="LogoImg"></img>
+      <div class="Headers" id="PCTypesContainer">
+        <buttom class="Headers PCTypesContainerClass" id="Mercury"><span id="MercurySpan">Mercury</span></buttom>
+        <buttom class="Headers PCTypesContainerClass" id="Jupiter"><span id="JupiterSpan">Jupiter</span></buttom>
+        <buttom class="Headers PCTypesContainerClass" id="Neptune"><span id="NeptuneSpan">Neptune</span></buttom>
+      </div>
+      <buttom class="Headers" id="SiteShopButtom">Buy</buttom>
     </header>
   );
 }
