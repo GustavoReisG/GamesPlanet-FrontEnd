d525cd417c4db97bdb5af892e3415598bc6fe113
Gustavo Reis <GustavoReisG17@outlook.com>
2024-04-12 15:28:03 -0300

    Criação da aplicação em React JS - Primeiros Passos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/js/header';
+
+function App() {
+  return (
+    <div className="MainDiv">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/js/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Definição do componente Header
+function Header() {
+  return (
+    <header>
+      <img></img>
+    </header>
+  );
+}
+
+export default Header;
